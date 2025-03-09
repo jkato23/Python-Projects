782fca9f4b5e2ae62733219e1cb1d7b5bd236813
jkato23 <jgmkato@gmail.com>
2025-03-08 21:15:37 -0800

    Delete coding_in_python directory

--- coding_in_python/os_example.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python3
-
-import os
-
-print("Your current working directory is: " + os.getcwd() + "\n\n")
-print("The contents of this directory are: ")
-os.system('ls')
-
-os.rename("first.txt", "second.txt")
